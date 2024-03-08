f8d08cf8296d1264abb00a7f4e7dd9c961a30b77
handlename <nagata@handlena.me>
2024-03-08 16:58:31 +0900

    feat: [BREAKING CHANGE] decide mode on runtime

--- cmd/let-rds-sleep/main.go
+++ cmd/let-rds-sleep/main.go
@@ -24,7 +24,7 @@ func main() {
 		flagVersion bool
 	)
 
-	flag.StringVar(&flagMode, "mode", "", "STOP or START")
+	flag.StringVar(&flagMode, "mode", "", "STOP or START, only for oneshot mode")
 	flag.StringVar(&flagTarget, "target", "", "TagName=Value,... If no tags given, treat all of resources as target")
 	flag.StringVar(&flagExclude, "exclude", "", "TagName=Value,... If Tag exists exclude the resource")
 	flag.BoolVar(&flagDryRun, "dryrun", false, "show process target only")
@@ -37,7 +37,6 @@ func main() {
 	}
 
 	app, err := lrs.New(
-		flagMode,
 		lrs.OptTarget(flagTarget),
 		lrs.OptExclude(flagExclude),
 		lrs.OptDryRun(flagDryRun),
@@ -48,11 +47,27 @@ func main() {
 	}
 
 	if strings.HasPrefix(os.Getenv("AWS_EXECUTION_ENV"), "AWS_Lambda") || os.Getenv("AWS_LAMBDA_RUNTIME_API") != "" {
+		if flagMode != "" {
+			fmt.Fprintf(os.Stderr, "-mode option is not available in Lambda mode")
+		}
+
 		fmt.Println("started as Lambda function handler")
 		lambda.Start(app.HandleRequest)
 	} else {
-		fmt.Println("started as oneshot app")
-		if err := app.Run(context.Background()); err != nil {
+		fmt.Println("started as oneshot mode")
+
+		var mode string
+		switch strings.ToUpper(flagMode) {
+		case "STOP":
+			mode = lrs.ModeStop
+		case "START":
+			mode = lrs.ModeStart
+		default:
+			fmt.Fprintf(os.Stderr, "invalid mode: %s", flagMode)
+			os.Exit(1)
+		}
+
+		if err := app.Run(context.Background(), mode); err != nil {
 			fmt.Fprintf(os.Stderr, "failed to Run: %s", err)
 			os.Exit(1)
 		}
