462b2f305adb8d15fc7c87c96dd0a36587aecd8d
handlename <nagata@handlena.me>
2024-03-15 17:50:18 +0900

    refactor: split RunCLI

--- cli.go
+++ cli.go
@@ -4,14 +4,11 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"strings"
 
 	"github.com/aws/aws-lambda-go/lambda"
-)
-
-var (
-	version string
 )
 
 const (
@@ -19,69 +16,92 @@ const (
 	ExitStatusError = 1
 )
 
-func RunCLI() int {
-	var (
-		flagMode    string
-		flagTarget  string
-		flagExclude string
-		flagDryRun  bool
-		flagVersion bool
-	)
+type CLIFlags struct {
+	Mode    string
+	Target  string
+	Exclude string
+	DryRun  bool
+	Version bool
+}
 
-	flag.StringVar(&flagMode, "mode", "", "STOP or START, only for oneshot mode")
-	flag.StringVar(&flagTarget, "target", "", "TagName=Value,... If no tags given, treat all of resources as target")
-	flag.StringVar(&flagExclude, "exclude", "", "TagName=Value,... If Tag exists exclude the resource")
-	flag.BoolVar(&flagDryRun, "dryrun", false, "show process target only")
-	flag.BoolVar(&flagVersion, "version", false, "display version")
+func RunCLI() int {
+	flags := parseCLIFlags()
+	if err := validateCLIFlags(flags); err != nil {
+		log.Printf("failed to validate flags: %s", err)
+		return ExitStatusError
+	}
+
+	if flags.Version {
+		fmt.Printf("let-rds-sleep %s", version)
+		return ExitStatusOK
+	}
+
+	if err := runApp(flags); err != nil {
+		log.Printf("[INFO] failed to run: %s", err)
+		return ExitStatusError
+	}
+
+	log.Println("[INFO] bye")
+
+	return ExitStatusOK
+}
+
+func isLambda() bool {
+	return strings.HasPrefix(os.Getenv("AWS_EXECUTION_ENV"), "AWS_Lambda") ||
+		os.Getenv("AWS_LAMBDA_RUNTIME_API") != ""
+}
+
+func parseCLIFlags() *CLIFlags {
+	flags := &CLIFlags{}
+
+	flag.StringVar(&flags.Mode, "mode", "", "STOP or START, only for oneshot mode")
+	flag.StringVar(&flags.Target, "target", "", "TagName=Value,... If no tags given, treat all of resources as target")
+	flag.StringVar(&flags.Exclude, "exclude", "", "TagName=Value,... If Tag exists exclude the resource")
+	flag.BoolVar(&flags.DryRun, "dryrun", false, "show process target only")
+	flag.BoolVar(&flags.Version, "version", false, "display version")
 	flag.Parse()
 
-	if flagVersion {
-		fmt.Printf("let-rds-sleep %s", version)
-		return ExitStatusError
+	return flags
+}
+
+func validateCLIFlags(flags *CLIFlags) error {
+	if isLambda() && flags.Mode != "" {
+		return fmt.Errorf("-mode option is only available in Lambda mode")
 	}
 
+	return nil
+}
+
+func runApp(flags *CLIFlags) error {
 	app, err := New(
-		OptTarget(flagTarget),
-		OptExclude(flagExclude),
-		OptDryRun(flagDryRun),
+		OptTarget(flags.Target),
+		OptExclude(flags.Exclude),
+		OptDryRun(flags.DryRun),
 	)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to init app: %s", err)
-		return ExitStatusError
+		return fmt.Errorf("failed to init app: %s", err)
 	}
 
-	if strings.HasPrefix(os.Getenv("AWS_EXECUTION_ENV"), "AWS_Lambda") || os.Getenv("AWS_LAMBDA_RUNTIME_API") != "" {
-		if flagMode != "" {
-			fmt.Fprintf(os.Stderr, "-mode option is not available in Lambda mode")
-		}
-
-		fmt.Println("started as Lambda function handler")
+	if isLambda() {
+		log.Println("[INFO] started as Lambda function handler")
 		lambda.Start(app.HandleRequest)
 	} else {
-		fmt.Println("started as oneshot mode")
+		log.Println("[INFO] started as oneshot mode")
 
 		var mode string
-		switch strings.ToUpper(flagMode) {
+		switch strings.ToUpper(flags.Mode) {
 		case "STOP":
 			mode = ModeStop
 		case "START":
 			mode = ModeStart
 		default:
-			fmt.Fprintf(os.Stderr, "invalid mode: %s", flagMode)
-			return ExitStatusError
+			return fmt.Errorf("invalid mode: %s", flags.Mode)
 		}
 
 		if err := app.Run(context.Background(), mode); err != nil {
-			fmt.Fprintf(os.Stderr, "failed to Run: %s", err)
-			return ExitStatusError
+			return fmt.Errorf("failed to Run: %s", err)
 		}
 	}
 
-	fmt.Println("bye")
-
-	return ExitStatusOK
-}
-
-func parseCLIArgs() {
-
+	return nil
 }
